e704999017bc5cc7c1d8de704368f4248320a9f0
Joseph <josephgreat2003@gmail.com>
2022-10-09 14:14:33 -0700

    pushed all files

--- bytescode/12.m
+++ bytescode/12.m
@@ -2,6 +2,17 @@ push 1
 # add
 push 2
 push 3
+push 4
+push 5
 pall
 add
+pall
+push 6
+push 7
+pall
+mul
+pall
+mod
+pall
+div
 pall
---
+++ bytescode/20.m
@@ -0,0 +1,12 @@
+# pchar
+push 0
+push 30
+push 100
+pchar
+push 10
+push 111
+push 109
+push 100
+push 55
+pall
+pstr
